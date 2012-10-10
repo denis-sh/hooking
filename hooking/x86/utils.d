7f6c0418d0808557578b4a95e677d121fd114fe8
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-10-10 21:55:17 +0400

    Add `hooking.x86.utils.get{Absolute|Relative}Target`

--- hooking/x86/utils.d
+++ hooking/x86/utils.d
@@ -10,6 +10,15 @@ module hooking.x86.utils;
 
 import std.traits: Signed;
 
+inout(void)* getAbsoluteTarget(inout(void)* address) nothrow
+{
+	return *cast(inout(void)**) address;
+}
+
+inout(void)* getRelativeTarget(inout(void)* address) nothrow
+{
+	return address + (void*).sizeof + *cast(Signed!size_t*) address;
+}
 
 /// Find a code sequence and return the address after the sequence
 inout(void)* findCodeSequence(inout(void)* startAddress, size_t len, string pattern) nothrow
@@ -25,6 +34,6 @@ inout(void)* findCodeSequence(inout(void)* startAddress, size_t len, string patt
 inout(void)* findCodeReference(inout(void)* startAddress, size_t len, string pattern, bool relative) nothrow
 {
 	if(auto p = findCodeSequence(startAddress, len, pattern))
-		return relative ? p + (void*).sizeof + *cast(Signed!size_t*) p : *cast(inout(void)**) p;
+		return relative ? getRelativeTarget(p) : getAbsoluteTarget(p);
 	return null;
 }
