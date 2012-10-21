fbd11657120fc4b2c967f6371a3b34c2d0957882
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-10-21 12:34:32 +0400

    Create `hooking.windows.heap` module

---
+++ hooking/windows/heap.d
@@ -0,0 +1,60 @@
+﻿/** Functions for heap manipulation
+
+Copyright: Denis Shelomovskij 2012
+
+License: $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Denis Shelomovskij
+*/
+module hooking.windows.heap;
+
+import core.sys.windows.windows;
+import std.exception;
+
+
+/// Returns default heap of the calling process.
+@property Heap processHeap()
+{ return Heap(GetProcessHeap()); }
+
+
+/// This struct encapsulates heap manipulation functionality.
+struct Heap
+{
+	private HANDLE _handle;
+
+	@property HANDLE handle()
+	{ return _handle; }
+
+	T[] alloc(T = void)(size_t count, DWORD flags = 0)
+	{
+		static if(T.sizeof == 1)
+			alias count bytes;
+		else
+		{
+			immutable size_t bytes = count * T.sizeof;
+			enforce(bytes / T.sizeof == count);
+		}
+		return enforce(cast(T*) HeapAlloc(_handle, flags, bytes))[0 .. count];
+	}
+
+	void free(void* p, DWORD flags = 0)
+	{
+		BOOL res = HeapFree(_handle, flags, p);
+		// Workaround bug mentioned in "Community Additions" section of
+		// http://msdn.microsoft.com/en-us/library/windows/desktop/aa366701(v=vs.85).aspx
+		if(cast(ubyte) GetVersion() < 6) // Not Windows Vista or later
+			res = cast(ubyte) res;
+		enforce(res);
+	}
+}
+
+
+// WinAPI
+// --------------------------------------------------
+
+extern(Windows) nothrow
+{
+	extern HANDLE GetProcessHeap();
+	extern PVOID HeapAlloc(PVOID HeapHandle, ULONG Flags, SIZE_T Size);
+	extern BOOL HeapFree(HANDLE hHeap, DWORD dwFlags, LPVOID lpMem);
+}
