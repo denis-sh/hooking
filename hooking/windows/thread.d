0a38b4531c0be2378eee091a54ba2cc497d73bbe
Denis Shelomovskij <verylonglogin.reg@gmail.com>
2012-10-31 21:10:32 +0400

    Add `hooking.windows.thread` documentation

--- hooking/windows/thread.d
+++ hooking/windows/thread.d
@@ -12,6 +12,7 @@ import hooking.windows.c.winternl;
 import core.sys.windows.windows;
 import std.exception;
 
+
 /** Returns whether $(D thread) is accosiated with a thread.
 It is asserted that no member functions are called for an unassociated
 $(D Thread) struct.
@@ -31,15 +32,19 @@ unittest
 }
 
 
+/** This struct encapsulates thread manipulation functionality.
+*/
 struct Thread
 {
 	package HANDLE _handle;
 	package DWORD _threadId;
 
+
 	invariant()
 	{ assert(this.associated, "Attempting to use unassociated Thread struct"); }
 
 
+	/// Construct a $(D Thread) from a $(D threadHandle).
 	this(HANDLE threadHandle)
 	{
 		_handle = threadHandle;
@@ -47,21 +52,41 @@ struct Thread
 	}
 
 
+	/// Gets the native _handle.
 	@property HANDLE handle()
 	{ return _handle; }
 
+
+	/// Gets the thread identifier.
 	@property DWORD threadId() const
 	{ return _threadId; }
 
+
+	/// Gets the process identifier of the owner process.
 	@property DWORD ownerProcessId()
 	{ return enforce(getThreadOrProcessIdOfThread(_handle, true)); }
 
+
+	/** Suspends thread.
+
+	Calls
+	$(HTTP msdn.microsoft.com/en-us/library/windows/desktop/ms686345(v=vs.85).aspx,
+	SuspendThread).
+	*/
 	void suspend() { enforce(SuspendThread(_handle) != -1); }
 
+
+	/** Resumes thread.
+
+	Calls
+	$(HTTP msdn.microsoft.com/en-us/library/windows/desktop/ms685086(v=vs.85).aspx,
+	ResumeThread).
+	*/
 	void resume() { enforce(ResumeThread(_handle) != -1); }
 
 
 	/** Waits for thread's EIP to be fixed on $(D address) (e.g. because of a `JMP $-2;` loop).
+
 	It will resume the thread if it is suspended and then increase suspended count with the same value.
 	*/
 	void executeUntil(size_t address)
@@ -85,6 +110,13 @@ struct Thread
 			suspend();
 	}
 
+
+	/** Gets thread context.
+	
+	Calls
+	$(HTTP msdn.microsoft.com/en-us/library/windows/desktop/ms679362(v=vs.85).aspx,
+	GetThreadContext).
+	*/
 	CONTEXT getContext(DWORD flags)
 	{
 		CONTEXT context;
@@ -93,11 +125,20 @@ struct Thread
 		return context;
 	}
 
+
+	/** Sets thread context.
+
+	Calls
+	$(HTTP msdn.microsoft.com/en-us/library/windows/desktop/ms680632(v=vs.85).aspx,
+	SetThreadContext).
+	*/
 	void setContext(CONTEXT context)
 	{
 		enforce(SetThreadContext(_handle, &context));
 	}
 
+	
+	/// Convenient function for changing thread context.
 	void changeContext(DWORD getFlags, scope void delegate(ref CONTEXT) del)
 	{
 		CONTEXT context = getContext(getFlags);
@@ -106,6 +147,10 @@ struct Thread
 	}
 }
 
+
+/** Closes $(D thread) handle if any.
+$(D thread) may be unassociated.
+*/
 void closeHandle(ref Thread thread)
 {
 	if(thread._handle)
